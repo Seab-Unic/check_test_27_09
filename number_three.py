3181d445bf47c9372cb05a2221dad0945d4e0401
Seaborgium_element <evskv42@yandex.ru>
2025-09-27 13:06:08 +0700

    Add files via upload

---
+++ number_three.py
@@ -0,0 +1,33 @@
+ear_time=input("какое сейчас время года")
+if ear_time == "зима":
+    winter=input("зима холодная или тёплая")
+    if winter == "холодная":
+        print("одень пуховик")
+    elif winter == "тёплая":
+        print("одень куртку")
+    else:
+        print("пиши правильно!!!")
+if ear_time == "лето":
+    winter=input("лето холодное или тёплое")
+    if winter == "холодное":
+        print("одень майку с длинным руковом")
+    elif winter == "тёплое":
+        print("одень майку")
+    else:
+        print("пиши правильно!!!")
+if ear_time == "осень":
+    winter=input("осень холодная или тёплая")
+    if winter == "холодная":
+        print("одень кофту")
+    elif winter == "тёплая":
+        print("одень майку с длинным рукавом")
+    else:
+        print("пиши правильно!!!")
+if ear_time == "весна":
+    winter=input("весна холодная или тёплая")
+    if winter == "холодная":
+        print("одень куртку")
+    elif winter == "тёплая":
+        print("одень ветровку")
+    else:
+        print("пиши правильно!!!")
